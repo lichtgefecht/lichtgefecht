b679fa6c81381444187a0654690bddfabd86824f
Marco Amann <marco@amann.dev>
2025-01-12 20:11:27 +0100

    missing changes

--- reflector/reflector/src/codec/mod.rs
+++ reflector/reflector/src/codec/mod.rs
@@ -1,21 +1,3 @@
 pub mod lichtgefecht {
     include!(concat!(env!("OUT_DIR"), "/lichtgefecht.rs"));
 }
-
-// use lichtgefecht::what;
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use prost::Message;
-    use std::io::Cursor;
-
-    #[test]
-    fn can_decode_own_encoding() {
-        let foo = lichtgefecht::Foo { foole: 42 };
-        let mut buf = Vec::new();
-        foo.encode(&mut buf).unwrap();
-        let bar = lichtgefecht::Foo::decode(&mut Cursor::new(buf)).unwrap();
-        assert!(bar.foole == 42);
-    }
-}
